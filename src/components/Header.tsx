6cf758bcf15e786f46e3807749cfc3210f54321c
HarishKalliyan <harishkalliyan@gmail.com>
2024-01-06 10:25:27 +0530

    Hosting settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
-    return (
-        <header className="header">
-            <img src="./images/troll-face.png" className="header--image" />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">Create Your Meme</h4>
-        </header>
-    )
+  return (
+    <header className="header">
+      <img src="./images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">Create Your Meme</h4>
+    </header>
+  );
 }
