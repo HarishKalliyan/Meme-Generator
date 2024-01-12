3a63fb9bb4a3baa3992e68135cd4697af4caa77a
HarishKalliyan <harishkalliyan@gmail.com>
2024-01-12 21:04:54 +0530

    New Desings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" className="header--image" />
+      <img src="images/memelogo.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">Create Your Meme</h4>
     </header>
